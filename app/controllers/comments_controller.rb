1aa436584ebafbfec936a68697faa92d59916460
Kyle Lortscher <kyle91302@gmail.com>
2015-04-15 15:38:15 -0700

    Made comments depedent added def destory

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,16 @@ class CommentsController < ApplicationController
 
 	def create
 		@post = Post.find(params[:post_id])
-		@comment = @post.comment.create(params[:comment].permit(:name, :body))
+		@comment = @post.comments.create(params[:comment].permit(:name, :body))
 
 		redirect_to post_path(@post)
+	end
+
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.commments.create(params[:id])
+		@comment.destory
+
+		redirect_to post_path
 	end	
 end
